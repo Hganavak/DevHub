88b7a9a16185f1995736eef0f733048fc20c07c4
Sam Kavanagh <hganavak@gmail.com>
2018-10-16 09:33:22 +1300

    Added a home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '**', redirectTo: '/home', pathMatch: 'full'}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { UserModule } from './Components/user/user.module';
+import { HomeComponent } from './Components/home/home.component';
+import { HomeModule } from './Components/home/home.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     UserModule,
+    HomeModule,
     AppRoutingModule
   ],
   bootstrap: [AppComponent]
